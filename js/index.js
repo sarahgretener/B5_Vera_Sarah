6456fd3ece40f2fa0045eae34f3059e4fd2c875b
verastutz05 <132079353+verastutz05@users.noreply.github.com>
2023-05-30 13:39:01 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,10 +17,10 @@ function showSlides(n) {
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 1; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 1; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex].style.display = "block";
